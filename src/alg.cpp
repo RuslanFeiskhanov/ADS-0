6e9f54e999ad1d8b52129c93a5ce2c963f64b527
RuslanFeiskhanov <99216168+RuslanFeiskhanov@users.noreply.github.com>
2022-02-13 14:07:13 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,20 +1,20 @@
 // Copyright 2021 NNTU-CS
 
 int gcd(int a, int b) {
-    int c=0;
-    bool f=0;
+    int c = 0;
+    bool f = 0;
     while (f == 0) {
         if (a >= b)
-        a = a-b;
+            a = a-b;
         else
-        a = b-a;
-        if(a%b == 0) {
-        c = b;  
-        f = 1;
+            a = b-a;
+        if (a%b == 0) {
+            c = b;  
+            f = 1;
         }
         if (b%a == 0) {
-        c = a;  
-        f = 1;
+            c = a;  
+            f = 1;
         }
     }
     return c;

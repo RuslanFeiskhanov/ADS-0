17af91ef7abaa7a516ae30de35f4d0b3791cb8d3
RuslanFeiskhanov <99216168+RuslanFeiskhanov@users.noreply.github.com>
2022-02-13 13:06:24 +0300

    Update and rename src/main.cpp to Src/main.cpp

---
+++ Src/main.cpp
@@ -0,0 +1,31 @@
+// Copyright 2021 NNTU-CS
+#include "alg.h"
+int gcd(int a, int b)
+{
+    int c=0;
+    bool f=0;
+    
+    while (f == 0)
+    {
+        if (a >= b)
+        a = a-b;
+        else
+        a = b-a;
+        
+        if(a%b == 0) 
+        {
+        c = b;  
+        f = 1;
+        }
+        if (b%a == 0)
+        {
+        c = a;  
+        f = 1;
+        }
+    }
+    return c;
+}
+int main() {
+    int result = gcd(10, 15);
+    return 0;
+}
--- src/main.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright 2021 NNTU-CS
-#include "alg.h"
-
-int main() {
-    int result = gcd(10, 15);
-    return 0;
-}
